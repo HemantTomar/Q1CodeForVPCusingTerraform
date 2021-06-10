a1efbf7d2ea5f8f9ac9adc4b2e044b501f95df0f
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2021-06-10 10:51:42 +0530

    Update main.tf

--- app01/dev/main.tf
+++ app01/dev/main.tf
@@ -1,14 +1,17 @@
 provider "aws" {
+  ##using variable for region if u want it it hard coded then uncomment it
   # region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  ## i have used ec2 role for tarraform credentials authentication
+  ## if u want to use Static credentials then uncomment below 2 lines
+  ##access_key = var.access_key
+  ##secret_key = var.secret_key
 }
 
 module "vpc" {
   source        = "../../modules/vpc"
-  vpc_cidr      = "10.0.0.0/16"
-  public_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  vpc_cidr      = "10.0.0.0/26"
+  public_cidrs  = ["10.0.0.0/28", "10.0.0.16/28"]
+  private_cidrs = ["10.0.0.32/28", "10.0.0.48/28"]
   
 }
 
